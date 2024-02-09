59132e5a88ec393c00e16920230803e293e36dee
buyshow <ayatbaeshev@gmail.com>
2024-02-09 20:32:11 +0100

    added api requests to the backend to get, add, delete and update feedbacks

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,13 +2,15 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { useContext } from 'react';
 import FeedbackItem from './FeedbackItem.jsx';
 import FeedbackContext from '../context/FeedbackContext.jsx';
+import Spinner from './shared/Spinner.jsx';
 export default function FeedbackList() {
-    const { feedback } = useContext(FeedbackContext);
+    const { feedback, isLoading } = useContext(FeedbackContext);
 
-    if (!feedback || feedback.length === 0) {
+    if (!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No feedback yet</p>
     }
-    return (
+
+    return isLoading ? <Spinner /> :
         <div className='feedback-list'>
             <AnimatePresence>
                 {feedback.map((item) => (
@@ -26,7 +28,7 @@ export default function FeedbackList() {
             </AnimatePresence>
 
         </div>
-    );
+
 
 }
 
